d4af523c470fdfd44fd7d3246289a805ea0d3e75
Arnav Bendre <arnav_b@hre.iitr.ac.in>
2026-09-16 23:00:58 +0530

    test for the kernel

--- fused_softmax.py
+++ fused_softmax.py
@@ -6,7 +6,7 @@ device = torch.device(f'cuda:{torch.cuda.current_device()}')
 
 # step 1
 def naive_softmax(x):
-    # assume x shape (M,N)---> here we are summing across the columns 
+    # assume x shape (M,N)---> here we are summing across the columns, also x resides in the DRAM
 
     # read MN elements and write M elements
     x_max = x.max (dim=1)[0] # shape (M)
@@ -18,3 +18,13 @@ def naive_softmax(x):
     out = numerator/ denominator[:, None] # read MN + M elements, write MN
 
     return out # in total, we did 8MN + 4M memory operations
+
+# step 2 
+def test_softmax_kernel(size: tuple, atol=1e-3, rtol=1e-3, device=device):
+    torch.manual_seed(0)
+    x=torch.randn(size[0],size[1],device=device)
+    z_tri = softmax(x)
+    z_torch = torch.softmax(x,axis = 1)
+    torch.testing.assert_close(z_tri, z_torch, atol=atol, rtol=rtol)
+    print ("PASSED")
+
